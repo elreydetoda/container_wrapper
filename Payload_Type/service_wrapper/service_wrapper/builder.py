472d34c9e3aa7a64f191529be754a8ddde666471
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2026-01-18 14:19:20 -0500

    initial MVP working

--- Payload_Type/service_wrapper/service_wrapper/builder.py
+++ Payload_Type/service_wrapper/service_wrapper/builder.py
@@ -4,7 +4,7 @@ import asyncio
 import os
 import tempfile
 from distutils.dir_util import copy_tree
-from pathlib import PurePath
+from pathlib import PurePath, Path
 import base64
 from mythic_container.MythicRPC import *
 
@@ -12,27 +12,37 @@ from mythic_container.MythicRPC import *
 class ServiceWrapper(PayloadType):
 
     name = "service_wrapper"
-    file_extension = "exe"
-    author = "@its_a_feature_"
-    supported_os = [SupportedOS.Windows]
+    file_extension = "tar"
+    author = "@elreydetoda"
+    supported_os = [SupportedOS.Linux]
     wrapper = True
     wrapped_payloads = []
-    note = "This is a wrapper payload that takes in Raw shellcode and generates a .NET Service binary. The service does not perform any injection."
+    note = "This is a wrapper payload that takes in a Linux executable payload and generates a docker container file (i.e. a tar.gz file)."
     supports_dynamic_loading = False
     build_parameters = [
         BuildParameter(
-            name="version",
-            parameter_type=BuildParameterType.ChooseOne,
-            description="Choose a target .NET Framework",
-            choices=["3.5", "4.0"],
-            default_value="4.0"
+            name="base_image",
+            parameter_type=BuildParameterType.String,
+            description="Choose a base container image (e.g. docker.io/ubuntu:24.04 OR scratch)",
+            default_value="docker.io/ubuntu:24.04"
         ),
         BuildParameter(
-            name="arch",
-            parameter_type=BuildParameterType.ChooseOne,
-            choices=["x64", "Any CPU"],
-            default_value="x64",
-            description="Target architecture",
+            name="entrypoint_file_name",
+            parameter_type=BuildParameterType.String,
+            default_value="entrypoint",
+            description="Binary that will be run when the container starts up",
+        ),
+        BuildParameter(
+            name="container_image_name",
+            parameter_type=BuildParameterType.String,
+            default_value="oci_container_image",
+            description="The name of the container image you want to export",
+        ),
+        BuildParameter(
+            name="container_image_version",
+            parameter_type=BuildParameterType.String,
+            default_value="latest",
+            description="The version of the container image you want to export",
         )
     ]
     c2_profiles = []
@@ -40,8 +50,7 @@ class ServiceWrapper(PayloadType):
     agent_icon_path = agent_path / "service_wrapper.svg"
     agent_code_path = agent_path / "agent_code"
     build_steps = [
-        BuildStep(step_name="Gathering Files", step_description="Copying files to temp location"),
-        BuildStep(step_name="Checking", step_description="Checking for MZ header"),
+        BuildStep(step_name="Writing Payload", step_description="Writing payload to temp location"),
         BuildStep(step_name="Building", step_description="Compiling with nuget and msbuild")
     ]
 
@@ -51,72 +60,57 @@ class ServiceWrapper(PayloadType):
         resp = BuildResponse(status=BuildStatus.Error)
         output = ""
         try:
-            command = "nuget restore; msbuild"
-            command += " -p:TargetFrameworkVersion=v{} -p:OutputType=WinExe -p:Configuration='{}' -p:Platform='{}'".format(
-                "3.5" if self.get_parameter("version") == "3.5" else "4.0",
-                "Release",
-                "x64" if self.get_parameter("arch") == "x64" else "Any CPU",
-            )
-            agent_build_path = tempfile.TemporaryDirectory(suffix=self.uuid).name
-            # shutil to copy payload files over
-            copy_tree(str(self.agent_code_path), agent_build_path)
-            working_path = (
-                PurePath(agent_build_path)
-                / "WindowsService1"
-                / "Resources"
-                / "loader.bin"
-            )
-            with open(str(working_path), "wb") as f:
-                f.write(self.wrapped_payload)
-            await SendMythicRPCPayloadUpdatebuildStep(MythicRPCPayloadUpdateBuildStepMessage(
-                PayloadUUID=self.uuid,
-                StepName="Gathering Files",
-                StepStdout="Found all files for payload",
-                StepSuccess=True
-            ))
-            with open(str(working_path), "rb") as f:
-                header = f.read(2)
-                if header == b"\x4d\x5a":  # checking for MZ header of PE files
-                    resp.build_stderr = "Supplied payload is a PE instead of raw shellcode."
-                    await SendMythicRPCPayloadUpdatebuildStep(MythicRPCPayloadUpdateBuildStepMessage(
-                        PayloadUUID=self.uuid,
-                        StepName="Checking",
-                        StepStdout="Found leading MZ header - supplied file wasn't shellcode",
-                        StepSuccess=True
-                    ))
-                    return resp
-            await SendMythicRPCPayloadUpdatebuildStep(MythicRPCPayloadUpdateBuildStepMessage(
-                PayloadUUID=self.uuid,
-                StepName="Checking",
-                StepStdout="No leading MZ header for payload",
-                StepSuccess=True
-            ))
-            proc = await asyncio.create_subprocess_shell(
-                command,
-                stdout=asyncio.subprocess.PIPE,
-                stderr=asyncio.subprocess.PIPE,
-                cwd=agent_build_path,
-            )
-            stdout, stderr = await proc.communicate()
-            if stdout:
-                output += f"[stdout]\n{stdout.decode()}"
-            if stderr:
-                output += f"[stderr]\n{stderr.decode()}"
-            output_path = (
-                PurePath(agent_build_path)
-                / "WindowsService1"
-                / "bin"
-                / "Release"
-                / "WindowsService1.exe"
-            )
-            output_path = str(output_path)
-            if os.path.exists(output_path):
-                resp.payload = open(output_path, "rb").read()
-                resp.status = BuildStatus.Success
-                resp.build_message = "New Service Executable created!"
-            else:
-                resp.payload = b""
-                resp.build_stderr = output + "\n" + output_path
+            with tempfile.TemporaryDirectory(suffix=self.uuid) as tmpdir:
+                # agent_build_path = tempfile.TemporaryDirectory(suffix=self.uuid).name
+                agent_build_path = tmpdir
+                # write payload as the "entrypoint" binary, so the name isn't obvious
+                payload_bin = Path(agent_build_path, self.get_parameter("entrypoint_file_name"))
+                payload_bin.write_bytes(self.wrapped_payload)
+                await SendMythicRPCPayloadUpdatebuildStep(MythicRPCPayloadUpdateBuildStepMessage(
+                    PayloadUUID=self.uuid,
+                    StepName="Writing Payload",
+                    StepStdout="Wrote payload file",
+                    StepSuccess=True
+                ))
+                command = [
+                    f"newcontainer=$(buildah from {self.get_parameter('base_image')})",
+                    f"chmod +x {payload_bin}",
+                    f"buildah add $newcontainer {payload_bin}",
+                    f"buildah config --cmd /{payload_bin.name} $newcontainer",
+                    f"buildah commit $newcontainer {self.get_parameter('container_image_name')}:latest",
+                    "buildah push " + 
+                        f"{self.get_parameter('container_image_name')}:{self.get_parameter('container_image_version')} " +
+                        f"oci-archive:{Path(agent_build_path, str(self.get_parameter('container_image_name')) + '.tar')}:{self.get_parameter('container_image_version')}",
+                    # "buildah rm $newcontainer",
+                    # f"buildah rmi {self.get_parameter('container_image_name')}:{self.get_parameter('container_image_version')}"
+                ]
+                proc = await asyncio.create_subprocess_shell(
+                    " && ".join(command),
+                    stdout=asyncio.subprocess.PIPE,
+                    stderr=asyncio.subprocess.PIPE,
+                    cwd=agent_build_path,
+                )
+                stdout, stderr = await proc.communicate()
+                if stdout:
+                    output += f"[stdout]\n{stdout.decode()}"
+                if stderr:
+                    output += f"[stderr]\n{stderr.decode()}"
+                # output_path = (
+                #     PurePath(agent_build_path)
+                #     / "WindowsService1"
+                #     / "bin"
+                #     / "Release"
+                #     / "WindowsService1.exe"
+                # )
+                # output_path = str(output_path)
+                output_path = Path(agent_build_path, str(self.get_parameter('container_image_name')) + '.tar')
+                if os.path.exists(output_path):
+                    resp.payload = output_path.read_bytes()
+                    resp.status = BuildStatus.Success
+                    resp.build_message = f"{stdout=}\n{stderr=}"
+                else:
+                    resp.payload = b""
+                    resp.build_stderr = output + "\n" + str(output_path)
         except Exception as e:
             raise Exception(str(e) + "\n" + output)
         return resp
